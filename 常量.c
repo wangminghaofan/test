a05893bd54994d67b1695db3bb3131639a3f214b
wangminghaofan <c17784058515@163.com>
2025-10-19 17:12:24 +0800

    Add files via upload

---
+++ 常量.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX 10
+enum Things
+{
+  book,
+  schoolbag,
+  paper
+};
+int main()
+{
+  222;
+  "abd";
+  const int m = 4;
+  printf("%d %d", m, MAX);
+  return 0;
+}
