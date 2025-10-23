d6b5d154136659c04b4feb0c66e4f0885670902c
wangminghaofan <c17784058515@163.com>
2025-10-23 23:51:18 +0800

    Add files via upload

---
+++ 20251023-235013.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10] = {0};
+    printf("%d\n",sizeof(arr));
+    printf("%d\n",sizeof (arr[0]));
+    int sz = sizeof(arr) / sizeof (arr[0]);
+    printf("%d\n",sz);
+    return 0;
+}
